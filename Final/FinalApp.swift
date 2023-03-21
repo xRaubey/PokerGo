d9a4e2c12a304a6b735b8b7ade1b66b138cffc01
Yuqing Yang <amokevin@me.com>
2023-03-21 15:20:53 -0400

    Initial Commit

---
+++ Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by Yuqing Yang on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
